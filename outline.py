380cd97feb5824154af59e7371eede71c581438c
Gracelyn Wilcox <125421422+gracielynjames@users.noreply.github.com>
2024-03-11 15:12:24 -0500

    Create outline.py
    Black outline within the Cameron Logo

---
+++ outline.py
@@ -0,0 +1,66 @@
+# Blackoutline
+coord = 38, 75, 100, 38
+blackOutline = c.create_line(coord, fill = 'black', width = dubz)
+
+coord = 40, 73, 40, 218
+blackOutline1 = c.create_line(coord, fill = 'black', width = dubz)
+
+coord = 40, 215, 100, 250
+blackOutline2 = c.create_line(coord, fill = 'black', width = dubz)
+
+coord = 98, 250, 195, 250
+blackOutline3 = c.create_line(coord, fill = 'black', width = dubz)
+
+coord = 193, 250, 260, 215
+blackOutline4 = c.create_line(coord, fill = 'black', width = dubz)
+
+coord = 260, 218, 260, 160
+blackOutline5 = c.create_line(coord, fill = 'black', width = dubz)
+
+coord = 263, 160, 220, 160
+blackOutline6 = c.create_line(coord, fill = 'black', width = dubz)
+
+coord = 220, 157 , 220, 175
+blackOutline7 = c.create_line(coord, fill = 'black', width = dubz)
+
+coord = 223, 175, 180, 195
+blackOutline8 = c.create_line(coord, fill = 'black', width = dubz)
+
+coord = 182, 195, 150, 195
+blackOutline9 = c.create_line(coord, fill = 'black', width = dubz)
+
+coord = 152, 195, 130, 175
+blackOutline10 = c.create_line(coord, fill = 'black', width = dubz)
+
+coord = 130, 177, 130, 80
+blackOutline11 = c.create_line(coord, fill = 'black', width = dubz)
+
+coord = 127, 80, 180, 80
+blackOutline12 = c.create_line(coord, fill = 'black', width = dubz)
+
+coord = 182, 195, 150, 195
+blackOutline13 = c.create_line(coord, fill = 'black', width = dubz)
+
+coord = 178, 78, 219, 115
+blackOutline14 = c.create_line(coord, fill = 'black', width = dubz)
+
+coord = 219, 112, 219, 128
+blackOutline15 = c.create_line(coord, fill = 'black', width = dubz)
+
+coord = 216, 128, 260, 128
+blackOutline16 = c.create_line(coord, fill = 'black', width = dubz)
+
+coord = 260, 131, 260, 38
+blackOutline17 = c.create_line(coord, fill = 'black', width = dubz)
+
+coord = 263, 38, 218, 38
+blackOutline18 = c.create_line(coord, fill = 'black', width = dubz)
+
+coord = 218, 35, 218, 50
+blackOutline19 = c.create_line(coord, fill = 'black', width = dubz)
+
+coord = 221, 50, 190, 38
+blackOutline20 = c.create_line(coord, fill = 'black', width = dubz)
+
+coord = 191, 38, 99, 38
+blackOutline21 = c.create_line(coord, fill = 'black', width = dubz)
